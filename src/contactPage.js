e36dc0d06e92e65b520ed52bee61b597949b9cc9
Jonathan Shovak <shovakitup@gmail.com>
2022-11-01 11:16:06 -0500

    stored some thoughts on the bottom of this page will remove them

--- src/contactPage.js
+++ src/contactPage.js
@@ -18,3 +18,22 @@ mainBody.appendChild(goatMeat);
 mainBody.appendChild(hook);
 mainBody.appendChild(foodTestimony);
 contact.appendChild(mainBody);
+
+
+<div class="tab">
+  <button class="tablinks" onclick="openTab(event, 'home')" id="buttonHome">Home</button>
+  <button class="tablinks" onclick="openTab(event, 'menu')">Menu</button>
+  <button class="tablinks" onclick="openTab(event, 'contact')">Content</button>
+</div>
+
+<div id="home" class="tabcontent">
+  //import of homePage.js export `${homeDisplay()}`
+</div>
+
+<div id="menu" class="tabcontent">
+  //import of menuPage.js export `${menuDisplay()}`
+</div>
+
+<div id="contact" class="tabcontent">
+  //import of contactPage.js export `${contentDisplay()}`
+</div>
