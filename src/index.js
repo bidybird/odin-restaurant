a75bcf65791c4f4b6a03f0cbb00037b6cd0609bd
Jonathan Shovak <shovakitup@gmail.com>
2022-11-01 11:18:11 -0500

    put everything else aside from the function pageTopLoad into notes to see if it worked, it did will gradually add one function at a time after adding  event listeners to tabs

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import { pageTopDisplay } from "./pageTop";
-import { homeTabDisplay } from "./homePage";
-import { menuTAbDisplay } from "./menuPage";
-import { contactTabDisplay } from "./contactPage";
+import { pageTopLoad } from "./pageTop";
+//import { homeTabDisplay } from "./homePage";
+//import { menuTAbDisplay } from "./menuPage";
+//import { contactTabDisplay } from "./contactPage";
 
-function openTab(event, tabContentId) {
-  let i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tabContentId).style.display = "block";
-  event.currentTarget.className += " active";
-}
+pageTopLoad();
 
-document.getElementById("buttonHome").click();
+// function openTab(event, tabContentId) {
+//   let i, tabcontent, tablinks;
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+//   document.getElementById(tabContentId).style.display = "block";
+//   event.currentTarget.className += " active";
+// }
+
+// document.getElementById("buttonHome").click();
