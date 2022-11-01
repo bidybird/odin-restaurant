63661057bb15e6b6959b400665ab28260d957c96
Jonathan Shovak <shovakitup@gmail.com>
2022-11-01 12:36:14 -0500

    added first menu item divs

--- src/menuPage.js
+++ src/menuPage.js
@@ -1,20 +1,37 @@
-mainBody = document.createElement("div");
-mainBody.setAttribute("id", "mainBody");
+function menuTabDisplay() {
+  const mainBody = document.createElement("div");
+  mainBody.setAttribute("id", "mainBody");
 
-restaurantName = document.createElement("h2");
-restaurantName.setAttribute("id", "restaurantName");
+  const restaurantName = document.createElement("h2");
+  restaurantName.setAttribute("id", "restaurantName");
 
-goatMeat = document.createElement("img");
-goatMeat.setAttribute("id", "goatMeat");
+  const mItemOne = document.createElement("div");
+  mItemOne.setAttribute("id", "mItemOne");
 
-hook = document.createElement("h3");
-hook.setAttribute("id", "hook");
+  const textItemOne = document.createElement("div");
+  textItemOne.setAttribute("id", "textItemOne");
 
-foodTestimony = document.createElement("p");
-foodTestimony.setAttribute("id", "foodTestimony");
+  const headItemOne = document.createElement("h3");
+  headItemOne.setAttribute("id", "headItemOne");
+  headItemOne.textContent = "Goat Kebabs: $9.99";
 
-mainBody.appendChild(restaurantName);
-mainBody.appendChild(goatMeat);
-mainBody.appendChild(hook);
-mainBody.appendChild(foodTestimony);
-menu.appendChild(mainBody);
+  const paraItemOne = document.createElement("p");
+  paraItemOne.setAttribute("id", "paraItemOne");
+  paraItemOne.textContent = "Our signature meat on skewers.";
+
+  const imgItemOne = document.createElement("img");
+  imgItemOne.setAttribute("id", "imgItemOne");
+  imgItemOne.src =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQjSqKN2XoCTMZE7av6_hLMU7JvXcXmv-fYg&usqp=CAU";
+
+  mainBody.appendChild(restaurantName);
+  textItemOne.appendChild(headItemOne);
+  textItemOne.appendChild(paraItemOne);
+  mItemOne.appendChild(textItemOne);
+  mItemOne.appendChild(imgItemOne);
+  mainBody.appendChild(mItemOne);
+  //menu.appendChild(mainBody);
+  container.appendChild(mainBody);
+}
+
+export { menuTabDisplay };
