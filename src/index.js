46ff18194e58eac9287c14f3040083831d6f6b4b
Jonathan Shovak <shovakitup@gmail.com>
2022-11-01 22:58:57 -0500

    added the hip div and its first div element, wrote notes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+const container = document.querySelector("#container");
+
 import { pageTopLoad } from "./pageTop";
 import { homeTabDisplay } from "./homePage";
 import { menuTabDisplay } from "./menuPage";
 import { contactTabDisplay } from "./contactPage";
 
 pageTopLoad();
+
+//hip div attaches to skeleton
+const hip = document.createElement("div");
+hip.setAttribute("id", "hip");
+document.querySelector("#skeleton").appendChild(hip);
+
+//childOne div is added to the hip div then it is removed and replaced in the homeTab
+const childOne = document.createElement("div");
+hip.appendChild(childOne);
 
 //on click display the home tab
 document.querySelector("#buttonHome").addEventListener("click", homeTabDisplay);
@@ -17,4 +28,4 @@ document
   .addEventListener("click", contactTabDisplay);
 
 //simulate clicking the home page on initial run
-document.getElementById("buttonHome").click();
+homeTabDisplay();
